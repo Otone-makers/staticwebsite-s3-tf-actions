f495c5a95ec339b93abc771adf07f1dcb9d58c35
otone-makers <amakisi100@outlook.com>
2025-05-30 15:20:14 -0500

    twelveth commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 
-Hosting a Static Website Using Terraform and Github Action
+# Hosting a Static Website Using Terraform and Github Action
+
+1. Create an S3 bucket using Terraform.
+2. Configure the bucket to allow public read access.
+3. Create an S3 bucket policy that allows read-only access.
+4. Setup Github Actions Workflow .yml 
+4a. Configure and integrate AWS to Github OIDC.
+
+
+
 
 
--- main.tf
+++ main.tf
@@ -37,10 +37,11 @@ resource "aws_s3_bucket_policy" "host_bucket_policy" {
     "Version" : "2012-10-17",
     "Statement" : [
       {
+        "Sid" : "PublicReadGetObject",
         "Effect" : "Allow",
         "Principal" : "*",
-        "Action" : "s3:*",
-        "Resource": "arn:aws:s3:::${var.my_bucket_name}/*"
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${var.my_bucket_name}/*"
       }
     ]
   })
